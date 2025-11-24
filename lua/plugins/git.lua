a2c269823a21302292dd305b2b56b4b21eb4a042
Sravan <sravan@uchicago.edu>
2025-11-24 12:23:21 -0600

    Added leader keys for neogit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -10,5 +10,29 @@ return {
       "echasnovski/mini.pick",         -- optional
       "folke/snacks.nvim",             -- optional
     },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit Status" },
+
+      -- Popups (like Magit)
+      { "<leader>gc", function() require("neogit").open({ "commit" }) end,
+        desc = "Commit Popup" },
+      { "<leader>gp", function() require("neogit").open({ "push" }) end,
+        desc = "Push Popup" },
+      { "<leader>gf", function() require("neogit").open({ "pull" }) end,
+        desc = "Pull Popup" },
+
+      -- Logs
+      { "<leader>gl", function() require("neogit").open({ "log" }) end,
+        desc = "Log (cwd)" },
+
+      -- Diff / DiffView integration
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diff (Diffview)" },
+      { "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+
+      -- Fast commands
+      { "<leader>gs", "<cmd>Neogit kind=split<cr>", desc = "Status (split)" },
+      { "<leader>gb", "<cmd>Neogit branch<cr>", desc = "Branch manager" },
+    }
   },
 }
