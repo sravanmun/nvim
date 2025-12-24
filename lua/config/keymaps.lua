d60d239915f0d5ddf1b5e4222253987dd4364cda
Sravan <sravan@uchicago.edu>
2025-12-24 11:28:26 -0500

    added floating terminal, cleaned up lsp, and added  codex

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,37 +4,38 @@ local ui = require("util.ui")
 vim.keymap.set("n", "<leader>;", "<cmd>FzfLua commands<CR>", { desc = "Command palette" })
 
 -- edit config files
-vim.keymap.set('n', '<leader>ca', '<cmd>e ~/.config/alacritty/alacritty.toml<cr>',   { desc = 'alacritty' })
-vim.keymap.set('n', '<leader>ck', '<cmd>e ~/.config/kitty/kitty.conf<cr>',   { desc = 'kitty' })
-vim.keymap.set('n', '<leader>cz', '<cmd>e ~/.config/zsh/zshrc.sh<cr>',   { desc = 'zsh' })
-vim.keymap.set('n', '<leader>cb', '<cmd>e ~/.bashrc<cr>',   { desc = 'bash' })
-vim.keymap.set('n', '<leader>cv', function()
-  require("snacks").picker.files({ cwd = vim.fn.stdpath("config") })
-end, { desc = 'neovim' })
-
+vim.keymap.set("n", "<leader>ca", "<cmd>e ~/.config/alacritty/alacritty.toml<cr>", { desc = "alacritty" })
+vim.keymap.set("n", "<leader>ck", "<cmd>e ~/.config/kitty/kitty.conf<cr>", { desc = "kitty" })
+vim.keymap.set("n", "<leader>cz", "<cmd>e ~/.config/zsh/zshrc.sh<cr>", { desc = "zsh" })
+vim.keymap.set("n", "<leader>cb", "<cmd>e ~/.bashrc<cr>", { desc = "bash" })
+vim.keymap.set("n", "<leader>cv", function()
+	require("snacks").picker.files({ cwd = vim.fn.stdpath("config") })
+end, { desc = "neovim" })
 
 -- file
-vim.keymap.set("n", "<leader>fs", '<cmd>w<cr>', { desc = "Save File" })
+vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save File" })
 vim.keymap.set("n", "<leader>fd", function()
-  require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h") })
+	require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h") })
 end, { desc = "Find files in current dir" })
 
--- Lazy
-vim.keymap.set("n", "<leader>ll", function() require("lazy").home() end, { desc = "Lazy" })
-
 -- Mode
-vim.keymap.set("n", "<leader>mz", '<cmd>ZenMode<cr>', { desc = "zen mode" })
-vim.keymap.set("n", "<leader>mtm", '<cmd>Mtm<cr>', { desc = "markdown table" })
+vim.keymap.set("n", "<leader>mz", "<cmd>ZenMode<cr>", { desc = "zen mode" })
+vim.keymap.set("n", "<leader>mtm", "<cmd>Mtm<cr>", { desc = "markdown table" })
+
+
+-- Term
+vim.keymap.set("n", "<leader>tt", "<cmd>FloatermToggle<cr>", { desc = "Open Floating Terminals" })
+vim.keymap.set("n", "<C-t>", "<cmd>FloatermToggle<cr>", { desc = "Toggle floating terminal" })
+vim.keymap.set("t", "<C-t>", [[<C-\><C-n><cmd>FloatermToggle<cr>]], { desc = "Toggle floating terminal" })
 
 -- quit
-vim.keymap.set("n", "<leader>qq", "<cmd>q<cr>",  { desc = "quite" })
-
+vim.keymap.set("n", "<leader>qq", "<cmd>q<cr>", { desc = "quite" })
 
 -- Move to window using hjkl>
-vim.keymap.set('n', '<leader>wh', '<C-w>h', { desc = 'Switch to left window' })
-vim.keymap.set('n', '<leader>wj', '<C-w>j', { desc = 'Switch to lower window' })
-vim.keymap.set('n', '<leader>wk', '<C-w>k', { desc = 'Switch to upper window' })
-vim.keymap.set('n', '<leader>wl', '<C-w>l', { desc = 'Switch to right window' })
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Switch to left window" })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Switch to lower window" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Switch to upper window" })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Switch to right window" })
 -- Swap windows around
 vim.keymap.set("n", "<leader>wH", "<C-w>H", { desc = "Move window to far left" })
 vim.keymap.set("n", "<leader>wJ", "<C-w>J", { desc = "Move window to very bottom" })
@@ -51,14 +52,26 @@ vim.keymap.set("n", "<leader>wr", "<C-w>r", { desc = "Rotate windows down/right"
 vim.keymap.set("n", "<leader>wR", "<C-w>R", { desc = "Rotate windows up/left" })
 -- split windows
 vim.keymap.set("n", "<leader>wv", "<cmd>vsplit<cr>", { desc = "Vertical split" })
-vim.keymap.set("n", "<leader>ws", "<cmd>split<cr>",  { desc = "Horizontal split" })
+vim.keymap.set("n", "<leader>ws", "<cmd>split<cr>", { desc = "Horizontal split" })
 -- quite/close windows
-vim.keymap.set("n", "<leader>wq", "<cmd>q<cr>",  { desc = "close window" })
+vim.keymap.set("n", "<leader>wq", "<cmd>q<cr>", { desc = "close window" })
 vim.keymap.set("n", "<leader>wo", "<C-w>o", { desc = "Keep only current window" })
 
 -- ui
-vim.keymap.set("n", "<leader>un",  function() ui.toggle_option("number", true) end, { desc = "Toggle line numbers" })
-vim.keymap.set("n", "<leader>uN",  function() ui.toggle_option("relativenumber", true) end,  { desc = "Toggle line numbers (all)" })
+vim.keymap.set("n", "<leader>un", function()
+	ui.toggle_option("number", true)
+end, { desc = "Toggle line numbers" })
+vim.keymap.set("n", "<leader>uN", function()
+	ui.toggle_option("relativenumber", true)
+end, { desc = "Toggle line numbers (all)" })
+
 
 -- change background
-vim.keymap.set("n", "<leader>ub", function() ui.toggle_background() end, { desc = "Toggle background (dark/light)" })
+vim.keymap.set("n", "<leader>ub", function()
+	ui.toggle_background()
+end, { desc = "Toggle background (dark/light)" })
+
+
+-- stop issues with recording
+vim.keymap.set("n", "q", "<nop>")
+vim.keymap.set("n", "Q", "q")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,20 +5,17 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 0
 vim.opt.expandtab = true
 
-vim.opt.clipboard = "unnamedplus"   -- use the + register (system)
+vim.opt.clipboard = "unnamedplus" -- use the + register (system)
 
 -- color scheme
 vim.opt.termguicolors = true
-vim.o.background = "dark" -- or "light" for light mode
-local default_colorscheme = "gruvbox"
--- local default_colorscheme = "zenburn"
--- local default_colorscheme = "retrobox"
--- local default_colorscheme = "everforest"
+vim.o.background = "dark"
+local default_colorscheme = "everforest"
 
 -- load color scheme safely
 local ok, _ = pcall(vim.cmd.colorscheme, default_colorscheme)
 if not ok then
-  vim.notify("Colorscheme " .. default_colorscheme .. " not found!")
+	vim.notify("Colorscheme " .. default_colorscheme .. " not found!")
 end
 
 -- change split direction
@@ -27,16 +24,22 @@ vim.opt.splitbelow = true
 
 -- use Treesitter folds
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr   = "nvim_treesitter#foldexpr()"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 
 -- optional: start with folds open
 vim.opt.foldlevel = 99
 vim.opt.foldenable = true
 
 -- Make CursorHold fire quickly
-vim.o.updatetime = 500        -- 0.5s idle
+vim.o.updatetime = 500 -- 0.5s idle
 
 -- Always show sign column (helps confirm diagnostics exist)
 vim.o.signcolumn = "yes"
 
-
+-- file highlighting
+vim.filetype.add({
+	extension = {
+		seq = "yaml",
+		bcf = "toml",
+	},
+})
