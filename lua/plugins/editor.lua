9e78a761003ea0e4a07e041ac268add766f45613
Sravan <sravan@uchicago.edu>
2026-01-09 14:01:39 -0500

    got rid of AI. got rid of autcomplete for quotes for python and markdown

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -11,23 +11,52 @@ return {
 	-- { "RRethy/vim-illuminate", opts = { delay = 150 } },
 
 	-- auto pairs
-	{
-		"echasnovski/mini.pairs",
-		event = "VeryLazy",
-		opts = {
-			modes = { insert = true, command = true, terminal = false },
-			-- skip autopair when next character is one of these
-			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-			-- skip autopair when the cursor is inside these treesitter nodes
-			skip_ts = { "string" },
-			-- skip autopair when next character is closing pair
-			-- and there are more closing pairs than opening pairs
-			skip_unbalanced = true,
-			-- better deal with markdown code blocks
-			markdown = true,
-		},
-	},
+    -- TODO: coppied for chatgpt as a quick fix. Go through this carefully
+    {
+      "echasnovski/mini.pairs",
+      event = "VeryLazy",
+      opts = {
+        modes = { insert = true, command = true, terminal = false },
+        skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+        skip_ts = { "string" },
+        skip_unbalanced = true,
+        markdown = false,
+      },
+      config = function(_, opts)
+        require("mini.pairs").setup(opts)
 
+        local function literal_i(buf, ch)
+          vim.keymap.set("i", ch, function() return ch end, {
+            buffer = buf,
+            expr = true,
+            noremap = true,
+            silent = true,
+          })
+        end
+
+        local function apply(buf)
+          local ft = vim.bo[buf].filetype
+          if ft == "python" then
+            literal_i(buf, '"')     -- disable " autopair in python only
+          elseif ft == "markdown" then
+            literal_i(buf, "`")     -- disable ` autopair in markdown only
+          end
+        end
+
+        -- Apply when filetype is set (future buffers)
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = { "python", "markdown" },
+          callback = function(ev) apply(ev.buf) end,
+        })
+
+        -- Apply to buffers that already existed before mini.pairs loaded
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.api.nvim_buf_is_loaded(buf) then
+            apply(buf)
+          end
+        end
+      end,
+    },
 	-- zen mode
 	{
 		"folke/zen-mode.nvim",
